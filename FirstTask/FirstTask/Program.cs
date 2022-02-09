425cf5071ee059ef997c17e5a2992ca19cb1c962
Dauda <nightcourage@gmail.com>
2022-02-09 10:42:26 +0200

    first commit

---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int xPosition = -5;
+            uint position = 338;
+            char dot = '.';
+            float money = 4.5f;
+            double pi = 3.14;
+            string word = "World";
+            bool isWinter = true;
+            int sweets = 48;
+            string name = "Player";
+            float timer = 45.1f;
+        }
+    }
+}
