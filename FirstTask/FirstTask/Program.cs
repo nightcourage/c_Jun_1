c46cb8aec250ab45d27460e54a000f749ce9be5b
Dauda <nightcourage@gmail.com>
2022-02-12 01:42:40 +0200

    Console game

--- FirstTask/FirstTask/Program.cs
+++ FirstTask/FirstTask/Program.cs
@@ -6,16 +6,30 @@ namespace FirstTask
     {
         static void Main(string[] args)
         {
-            int xPosition = -5;
-            uint position = 338;
-            char dot = '.';
-            float money = 4.5f;
-            double pi = 3.14;
-            string word = "World";
-            bool isWinter = true;
-            int sweets = 48;
-            string name = "Player";
-            float timer = 45.1f;
+            //Вы задаете вопросы пользователю, по типу "как вас зовут", "какой ваш знак зодиака" и тд, после чего, по данным, которые он ввел,
+            //    формируете небольшой текст о пользователе.
+
+            //"Вас зовут Алексей, вам 21 год, вы водолей и работаете на заводе."
+
+            string firstName, lastName, profession, city;
+            int age, salary;
+
+            Console.Write("Укажите ваше имя: ");
+            firstName = Console.ReadLine();
+            Console.Write("Укажите вашу фамилию: ");
+            lastName = Console.ReadLine();
+            Console.Write("Укажите вашу профессию: ");
+            profession = Console.ReadLine();
+            Console.Write("Укажите город в котором проживаете: ");
+            city = Console.ReadLine();
+            Console.Write("Укажите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Укажите вашу зарплату в USD: ");
+            salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Вас зовут {firstName} {lastName}. Вы проживаете в городе {city}. Вам {age} лет.\nВы работатет {profession} и получаете {salary} USD");
+
+
         }
     }
 }
